61aebb1f6fde74b1c6219cbe64824af7d2b8fe7d
Jan Winter <jan.winter@dkfz-heidelberg.de>
2017-08-21 15:23:38 +0200

    no message

--- source/multiuser/ui.r
+++ source/multiuser/ui.r
@@ -182,7 +182,7 @@ sidebar <- dashboardSidebar(sidebarMenu(
 body <- dashboardBody(
   shiny::tags$head(
     shiny::tags$style(HTML(config$stylesheet)),
-    shiny::includeScript("tooltip-delay.js")
+    shiny::includeScript(file.path(config$wd,"tooltip-delay.js"))
   ),
   #busyIndicator(),
   
