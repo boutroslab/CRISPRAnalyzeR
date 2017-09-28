a61ad4b1d76992f00aeb85d1ceda43c85b723bb3
Jan Winter <jan.winter@dkfz-heidelberg.de>
2017-09-28 16:02:51 +0200

    fix typo in download handler for read count files

--- source/app/extraction_server.r
+++ source/app/extraction_server.r
@@ -747,7 +747,7 @@ output$download_readcounts <- downloadHandler(
     
     # Add FASTA File
     file.copy(file.path(userDir, "libFile"), file.path(userDir, paste(libFile()$name, ".fasta", sep="") ))
-    filestozip <- c(filestozip, fpaste(libFile()$name) )
+    filestozip <- c(filestozip, paste(libFile()$name) )
     
     arguments <- c("-czf", file.path(userDir, paste("Readcount_",userID,".tar.gz", sep="")), paste("-C", file.path(userDir), sep=" "), paste(filestozip, collapse = " "))
     
